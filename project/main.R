a498acdb961f9f13b617f68959d2deb516e9c0ac
Restyled.io <commits@restyled.io>
2020-05-23 07:53:55 +0000

    Restyled by whitespace

--- project/main.R
+++ project/main.R
@@ -56,7 +56,7 @@ nutrient_mean <- aggregate(amount ~ category + country, dataframe , FUN=mean)
 
 barplot(nutrient_sum$amount, names.arg = nutrient_sum$country, col = nutrient_sum$category)
 barplot(nutrient_mean$amount, names.arg = nutrient_mean$country, col = nutrient_mean$category)
-          
+
 # Number of products per country
 product_name_sum <- aggregate(product.name ~ country, dataframe , FUN=length)
 barplot(product_name_sum$product.name, names.arg = product_name_sum$country, col = product_name_sum$product.name)
