abb4982e2be072cadf3f40f2304b69c20ccce8ed
Patryk Gronkiewicz <gronek.konta@gmail.com>
2020-05-23 18:28:00 +0200

    reshape, uniformize units

--- project/main.R
+++ project/main.R
@@ -24,7 +24,7 @@ dataframe <- dataframe[interesting_cols]
 ## rename columns to more convenient names
 colnames(dataframe) <- c(
     "country",
-    "product name",
+    "product_name",
     "category",
     "subcategory",
     "subsubcategory",
@@ -38,7 +38,7 @@ dataframe[sample(nrow(dataframe), 20), ]
 ## convert to df then column to factors
 dataframe <- data.frame(dataframe)
 
-dataframe$product.name <- as.factor(dataframe$product.name)
+dataframe$product_name <- as.factor(dataframe$product_name)
 dataframe$category <- as.factor(dataframe$category)
 dataframe$country <- as.factor(dataframe$country)
 dataframe$subcategory <- as.factor(dataframe$subcategory)
@@ -46,18 +46,57 @@ dataframe$subsubcategory <- as.factor(dataframe$subsubcategory)
 dataframe$nutrient <- as.factor(dataframe$nutrient)
 dataframe$unit <- as.factor(dataframe$unit)
 
+dataframe$amount[dataframe$unit == "Microgram/100 gram"] <- dataframe$amount[dataframe$unit == "Microgram/100 gram"] / 1000
+
+## reshape df to use separate nutrients as columns
+dataframe <- reshape2::dcast(
+    dataframe,
+    country + product_name + category + subcategory + subsubcategory ~ nutrient,
+    value.var = "amount",
+    sum
+)
+
+numeric_cols <- c(
+    "Alpha-tocopherol",
+    "Calcium (Ca)",
+    "Copper (Cu)",
+    "Magnesium (Mg)",
+    "niacin equivalents, total",
+    "Phosphorus (P)",
+    "Potassium (K)",
+    "riboflavin",
+    "thiamin",
+    "Total iron",
+    "Total Selenium",
+    "vitamin B-12",
+    "vitamin B-6, total",
+    "vitamin E; alpha-tocopherol equiv from E vitamer activities",
+    "vitamin K, total",
+    "Zinc (Zn)"
+)
+
+dataframe[sample(nrow(dataframe), 10), ]
 
 ## plots
 
 # Sum and mean of nutrients per country
-nutrient_sum <- aggregate(amount ~ category + country, dataframe , FUN=sum)
-nutrient_mean <- aggregate(amount ~ category + country, dataframe , FUN=mean)
+nutrient_sum <- aggregate(amount ~ category + country, dataframe, FUN = sum)
+nutrient_mean <- aggregate(amount ~ category + country, dataframe, FUN = mean)
 
-barplot(nutrient_sum$amount, names.arg = nutrient_sum$country, col = nutrient_sum$category)
-barplot(nutrient_mean$amount, names.arg = nutrient_mean$country, col = nutrient_mean$category)
+barplot(nutrient_sum$amount,
+    names.arg = nutrient_sum$country,
+    col = nutrient_sum$category
+)
+barplot(nutrient_mean$amount,
+    names.arg = nutrient_mean$country,
+    col = nutrient_mean$category
+)
 
 # Number of products per country
-product_name_sum <- aggregate(product.name ~ country, dataframe , FUN=length)
-barplot(product_name_sum$product.name, names.arg = product_name_sum$country, col = product_name_sum$product.name)
+product_name_sum <- aggregate(product_name ~ country, dataframe, FUN = length)
+barplot(product_name_sum$product_name,
+    names.arg = product_name_sum$country,
+    col = product_name_sum$product_name
+)
 
 # Generate correlations
