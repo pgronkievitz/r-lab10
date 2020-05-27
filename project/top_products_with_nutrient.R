6b80ae82b2f36e38d9a119fb5378a9e331976772
finloop <>
2020-05-27 21:32:04 +0200

    add top prod with nutrient

---
+++ project/top_products_with_nutrient.R
@@ -0,0 +1,19 @@
+top_products_with_nutrient <- function(df, nutrient_name) {
+## Zwraca df posortowany tak, żeby prosukty które mają najwięcej
+## danego mikro/makro elementu znalazły się na górze + dodaje
+## index w celu ułatwienia interpretacji
+#
+# nutrient_name - nazwa składinka którego maxa poszukujemy
+#
+# NOTE: wymaga df w wersji WIDE
+  
+  # Posortowanie i wybranie odpowiednich kolumn
+  dat <- df[order(df[nutrient_name], decreasing = TRUE),c(1,2,3,4,5, which(colnames(df) == "Calcium (Ca)"))]
+  
+  # 
+  dat <- cbind(1:dim(dat)[1], dat)
+  colnames(dat)[1] <- "index"
+  return(dat)
+}
+
+# k <- top_products_with_nutrient(dataframe2, 'Alpha-tocopherol')
