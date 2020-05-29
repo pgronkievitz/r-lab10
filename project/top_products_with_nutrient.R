5d237e0535fbe1b96dffc778f149de304d4ad8d3
finloop <unknown>
2020-05-29 21:09:14 +0200

    fix fixed var

--- project/top_products_with_nutrient.R
+++ project/top_products_with_nutrient.R
@@ -8,7 +8,7 @@ top_products_with_nutrient <- function(df, nutrient_name) {
 # NOTE: wymaga df w wersji WIDE
   
   # Posortowanie i wybranie odpowiednich kolumn
-  dat <- df[order(df[nutrient_name], decreasing = TRUE),c(1,2,3,4,5, which(colnames(df) == "Calcium (Ca)"))]
+  dat <- df[order(df[nutrient_name], decreasing = TRUE),c(1,2,3,4,5, which(colnames(df) == nutrient_name))]
   
   # 
   dat <- cbind(1:dim(dat)[1], dat)
