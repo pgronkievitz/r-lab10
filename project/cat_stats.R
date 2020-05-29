dcf4dde849cd05024bff115a7667f2949c1fa0c3
Patryk Gronkiewicz <gronek.konta@gmail.com>
2020-05-29 20:30:38 +0200

    add category stats file

---
+++ project/cat_stats.R
@@ -0,0 +1,35 @@
+source("main.R")
+category_stats <- function(dataframe, category_of_products) {
+    stats <- list()
+    for (country in levels(dataframe$country)) {
+        all_products <- (nrow(dataframe[dataframe$country == country, ]))
+        cat_no <- (nrow(dataframe[dataframe$country == country &
+            dataframe$category == category_of_products, ]))
+        stats[[country]] <- (cat_no / all_products)
+    }
+
+    return(stats)
+}
+subcategory_stats <- function(dataframe, category_of_products) {
+    stats <- list()
+    for (country in levels(dataframe$country)) {
+        all_products <- (nrow(dataframe[dataframe$country == country, ]))
+        cat_no <- (nrow(dataframe[dataframe$country == country &
+            dataframe$subcategory == category_of_products, ]))
+        stats[[country]] <- (cat_no / all_products)
+    }
+
+    return(stats)
+}
+subsubcategory_stats <- function(dataframe, category_of_products) {
+    stats <- list()
+    for (country in levels(dataframe$country)) {
+        all_products <- (nrow(dataframe[dataframe$country == country, ]))
+        cat_no <- (nrow(dataframe[dataframe$country == country &
+            dataframe$subsubcategory == category_of_products, ]))
+        stats[[country]] <- (cat_no / all_products)
+    }
+
+    return(stats)
+}
+subcategory_stats(dataframe, "Water based beverages")
