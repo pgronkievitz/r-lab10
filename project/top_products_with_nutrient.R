f61cf53c80a2c18cf6a193d6fc120268bde56eae
Restyled.io <commits@restyled.io>
2020-05-29 19:10:33 +0000

    Restyled by whitespace

--- project/top_products_with_nutrient.R
+++ project/top_products_with_nutrient.R
@@ -6,11 +6,11 @@ top_products_with_nutrient <- function(df, nutrient_name) {
 # nutrient_name - nazwa składinka którego maxa poszukujemy
 #
 # NOTE: wymaga df w wersji WIDE
-  
+
   # Posortowanie i wybranie odpowiednich kolumn
   dat <- df[order(df[nutrient_name], decreasing = TRUE),c(1,2,3,4,5, which(colnames(df) == nutrient_name))]
-  
-  # 
+
+  #
   dat <- cbind(1:dim(dat)[1], dat)
   colnames(dat)[1] <- "index"
   return(dat)
